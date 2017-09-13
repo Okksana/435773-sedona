0bfc2b986ff9bea548941a342baab1f9f0e40567
Okksana <diabolomaxxximum@mail.ru>
2017-09-13 16:15:01 +0900

    Edit 9

--- js/script.js
+++ js/script.js
@@ -1,53 +1,32 @@
-﻿var modalLink, modalWindow, searchForm, arrival, departure, adults, children, filterForm, filterLink;
+﻿var modalLink, modalWindow, searchForm, arrival, departure, adults, children;
 
 modalLink = document.querySelector(".modal-button");
+modalWindow = document.querySelector(".modal");
+searchForm = modalWindow.querySelector(".search-form");
+arrival = modalWindow.querySelector("[name=arrival-date]");
+departure = modalWindow.querySelector("[name=departure-date]");
+adults = modalWindow.querySelector("[name=adults]");
+children = modalWindow.querySelector("[name=children]");
 
-if (modalLink != null) {
-  modalWindow = document.querySelector(".modal");
-  searchForm = modalWindow.querySelector(".search-form");
-  arrival = modalWindow.querySelector("[name=arrival-date]");
-  departure = modalWindow.querySelector("[name=departure-date]");
-  adults = modalWindow.querySelector("[name=adults]");
-  children = modalWindow.querySelector("[name=children]");
+  modalWindow.classList.add("modal-hidden");
 
-    modalWindow.classList.add("modal-hidden");
+  modalLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalWindow.classList.toggle("modal-hidden");
+    arrival.focus();
+    });
 
-    modalLink.addEventListener("click", function (evt) {
+  modalLink.addEventListener("keypress", function (evt) {
+    if (evt.keyCode === 32 || evt.keyCode === 13) {
       evt.preventDefault();
       modalWindow.classList.toggle("modal-hidden");
       arrival.focus();
-      });
-
-    modalLink.addEventListener("keypress", function (evt) {
-      if (evt.keyCode === 32 || evt.keyCode === 13) {
-        evt.preventDefault();
-        modalWindow.classList.toggle("modal-hidden");
-        arrival.focus();
-        }
-      });
-
-    searchForm.addEventListener("submit", function (evt) {
-      if (!arrival.value || !departure.value || !adults.value || !children.value) {
-        evt.preventDefault();
-        alert("Сперва нужно заполнить все поля!");
-        }
-      });
-  }
-
-else {
-
-  filterForm = document.querySelector(".filter");
-  filterLink = document.querySelector(".filter-button");
-
-    filterLink.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      filterForm.submit();
+      }
     });
 
-    filterLink.addEventListener("keypress", function (evt) {
-      if (evt.keyCode === 32 || evt.keyCode === 13) {
-        evt.preventDefault();
-        filterForm.submit();
-        }
-      });
-  }
+  searchForm.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !adults.value || !children.value) {
+      evt.preventDefault();
+      alert("Сперва нужно заполнить все поля!");
+      }
+    });
