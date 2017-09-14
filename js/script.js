e4ce47c83e706c01fcffff1ddd0472c78db0c557
Okksana <diabolomaxxximum@mail.ru>
2017-09-14 16:45:30 +0900

    Edit 11: animation of err

--- js/script.js
+++ js/script.js
@@ -8,25 +8,29 @@ departure = modalWindow.querySelector("[name=departure-date]");
 adults = modalWindow.querySelector("[name=adults]");
 children = modalWindow.querySelector("[name=children]");
 
-  modalWindow.classList.add("modal-hidden");
+modalWindow.classList.add("modal-hidden");
 
-  modalLink.addEventListener("click", function (evt) {
+modalLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalWindow.classList.toggle("modal-hidden");
+  modalWindow.classList.remove("modal-error");
+  arrival.focus();
+  });
+
+modalLink.addEventListener("keypress", function (evt) {
+  if (evt.keyCode === 32 || evt.keyCode === 13) {
     evt.preventDefault();
     modalWindow.classList.toggle("modal-hidden");
+    modalWindow.classList.remove("modal-error");
     arrival.focus();
-    });
+    }
+  });
 
-  modalLink.addEventListener("keypress", function (evt) {
-    if (evt.keyCode === 32 || evt.keyCode === 13) {
-      evt.preventDefault();
-      modalWindow.classList.toggle("modal-hidden");
-      arrival.focus();
-      }
-    });
-
-  searchForm.addEventListener("submit", function (evt) {
-    if (!arrival.value || !departure.value || !adults.value || !children.value) {
-      evt.preventDefault();
-      alert("Сперва нужно заполнить все поля!");
-      }
-    });
+searchForm.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    modalWindow.classList.remove("modal-error");
+    modalWindow.offsetWidth = modalWindow.offsetWidth;
+    modalWindow.classList.add("modal-error");
+    }
+  });
